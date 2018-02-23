90e98d2d356a25881a39a9ea28e938954ded864a
Dave Thomas <davethomas11@icloud.com>
2018-02-22 22:52:03 -0800

    Remove empty files

--- moshipack-retrofit/src/main/kotlin/MoshiPackRequestBodyConverter.kt
+++
@@ -1,7 +0,0 @@
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.MsgpackWriter
-import okhttp3.MediaType
-import okhttp3.RequestBody
-import okio.Buffer
-import retrofit2.Converter
-
--- moshipack-retrofit/src/main/kotlin/MoshiPackResponseBodyConverter.kt
+++
@@ -1,5 +0,0 @@
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.MsgpackReader
-import okhttp3.ResponseBody
-import retrofit2.Converter
-
